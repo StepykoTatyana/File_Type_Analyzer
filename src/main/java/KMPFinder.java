0420bf3f7bbc455f23a90d254953a0f326d9f4e6
Tanya <Sunnytanya93@mail.ru>
2022-05-14 12:40:21 +0700

    deleted the excess

--- src/main/java/KMPFinder.java
+++ src/main/java/KMPFinder.java
@@ -13,22 +13,12 @@ public class KMPFinder implements FindSubStringInterface {
         int k = 0;
         int j = 0;
         int m = 0;
-        long startTime = System.nanoTime();
         while (k != pattern.length) {
             for (int i = j; i < allBytes.length; i++) {
                 if (allBytes[i] == pattern[k]) {
                     k++;
                     if (k == pattern.length) {
-                        if (file != null) {
-                            //System.out.println(file.getName() + ": " + typeFile);
-                        } else {
-                            //System.out.println(typeFile);
-                        }
                         flagExist = true;
-
-                        long elapsedNanos = System.nanoTime() - startTime;
-//                        System.out.printf("It took %.3f seconds", (float) elapsedNanos / 1_000_000_000);
-//                        System.out.println();
                         break;
                     }
                 } else {
@@ -43,14 +33,6 @@ public class KMPFinder implements FindSubStringInterface {
             }
             m++;
             if (m > allBytes.length) {
-                if (file != null) {
-                    //System.out.println("!" + file.getName() + ": Unknown file type");
-                } else {
-                    //System.out.println("Unknown file type");
-                }
-                long elapsedNanos = System.nanoTime() - startTime;
-//                System.out.printf("It took %.3f seconds", (float) elapsedNanos / 1_000_000_000);
-//                System.out.println();
                 flagExist = false;
                 break;
             }
